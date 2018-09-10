583494aca0bc4bc5dca25499da52146438ccadcd
Natasha Nishida <nat.nishida@gmail.com>
2018-09-10 12:45:05 -0300

    contato

---
+++ contato.php
@@ -0,0 +1,167 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/style.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title>Cross 373</title>
+
+	<style>
+	.gm {
+		width: 50%;
+		float: left;
+		}
+
+	.gmint {
+		float: right;
+		margin-right: 8%;
+	}
+
+	.form {
+		float: left;
+		width: 50%;
+		padding-top: 20px;
+	}
+
+	.form form input {
+		width: 400px;
+		height: 30px;
+		margin-bottom: 8px;
+		padding-left: 10px;
+	}
+
+	.form form textarea {
+		width: 400px;
+		height: 180px;
+		margin-bottom: 8px;
+		padding-left: 10px;
+	}
+
+	</style>
+</head>
+<body>
+	<nav class="navbar fixed-top navbar-expand-lg navbar-dark  ">
+		<a class="navbar-brand" href="#">CROSS 373</a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+
+		<div class="collapse navbar-collapse" id="navbarSupportedContent">
+			<ul class="navbar-nav mr-auto">
+				<li class="nav-item active">
+					<a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="index.php#sobre">Sobre</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="index.php#horarios">Horários</a>
+				</li>
+				<li class="nav-item dropdown">
+					<a class="nav-link dropdown-toggle" href="store.php" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+						Store
+					</a>
+					<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+						<a class="dropdown-item" href="#">Planos</a>
+						<div class="dropdown-divider"></div>
+						<a class="dropdown-item" href="#">Produtos Fitness</a>
+					</div>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="contato.php">Contato</a>
+				</li>
+			</ul>
+			<ul class="navbar-nav mr-right">
+				<li class="nav-item">
+					<a class="nav-link" href="#">Cadastre-se</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">Login</a>
+				</li>
+			</ul>
+		</div>
+	</nav>
+
+
+	<div class="gm">
+		<div class="gmint">
+			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3655.15500612253!2d-46.602137384955746!3d-23.634619270186967!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce5b5c452b6593%3A0x873bb7d326cce2eb!2sAv.+Nossa+Sra.+da+Encarna%C3%A7%C3%A3o%2C+373+-+Sacom%C3%A3%2C+S%C3%A3o+Paulo+-+SP%2C+04180-080!5e0!3m2!1spt-BR!2sbr!4v1536350633232" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+		</div>
+	</div>
+	<div class="form">
+	<form method="POST" action="">
+		<input type="text" name="nome" placeholder="Nome" value="">
+		<br>
+		<input type="email" name="email" placeholder="E-Mail" value="">
+		<br>
+		<input type="text" name="number" placeholder="Telefone" value="">
+		<br>
+		<input type="text" name="text" placeholder="Assunto" value="">
+		<br>
+		<textarea type="text" placeholder="Mensagem" value="">
+		</textarea>
+		<br>
+		<button name="enviar">Enviar</button>
+	</form>
+
+	<div class="clear_both">
+
+	</div>
+	<footer class="container-fluid" style="clear:both">
+	    <div class="newsletter row">
+	      <div class="textonews col-lg-5">
+	          <p>Inscreva-se na nossa newsletter!</p>
+	      </div>
+
+	      <div class="col-lg-7">
+	        <form action="">
+	          <div class="camposnewsletter">
+	            <input type="text" placeholder="Name" name="name" required>
+	            <input type="text" placeholder="Email address" name="mail" required>
+	            <input class="btn btn-outline-light" type="submit" value="Inscreva-se">
+	          </div>
+	        </form>
+	      </div>
+	      </div>
+
+	      <div class="social">
+
+	        <img class="socialicon" src="img/instagram.png" alt="email para o cross373" width="40">
+	        <img class="socialicon" src="img/facebook.png" alt="email para o cross373" width="35">
+	        <img src="img/email.png" alt="email para o cross373" width="50">
+	      </div>
+	      <hr>
+	      <br>
+	      <div class="contatos">
+	        <div class="row">
+	          <div class="col-lg-6">
+	            <button class="btn btn-danger" type="button" name="button">Agendar Free Pass </button>
+	          </div>
+	          <div class="col-lg-6">
+	            <button class="btn btn-danger" type="button" name="button">Entre em Contato</button>
+	          </div>
+
+	        </div>
+	      </div>
+	      <hr>
+	      <br>
+	      <p>Telefone: (11) xxxxx-xxxx</p>
+	      <p>Endereço: Av. Nossa Sra. da Encarnação, 373. Jardim Maria Estela</p>
+	      <br>
+	      <hr>
+	      <br>
+
+
+	<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14620.619127815446!2d-46.5999536!3d-23.6346273!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x90450a21241cce54!2sAcademia+Nishida+Nata%C3%A7%C3%A3o!5e0!3m2!1spt-BR!2sbr!4v1536523586941" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+	</footer>
+
+
+	    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+	    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+	    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+
+	</div>
+</body>
+</html>
