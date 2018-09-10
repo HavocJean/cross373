4fd81313d342ac1efcb0c0ee18c89004b3224eff
Natasha Nishida <nat.nishida@gmail.com>
2018-09-10 12:48:22 -0300

    Site todo

---
+++ index.php
@@ -0,0 +1,312 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Cross 373</title>
+  </head>
+  <body>
+  <!-- Menu -->
+  <nav class="navbar fixed-top navbar-expand-lg navbar-dark  ">
+    <a class="navbar-brand" href="#">CROSS 373</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#sobre">Sobre</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#horarios">Horários</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="store.php" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Store
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="#">Planos</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="store.php">Produtos Fitness</a>
+          </div>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="contato.php">Contato</a>
+        </li>
+      </ul>
+      <ul class="navbar-nav mr-right">
+        <li class="nav-item">
+          <a class="nav-link" href="#">Cadastre-se</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Login</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+
+  <!-- Carrossel -->
+  <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+      <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img class="d-block w-100" src="img/banner1.png?auto=yes&bg=777&fg=555&text=First slide" alt="Cross373">
+        <div class="carousel-caption d-none d-md-block">
+          <h3><strong>Cross 373</strong></h3>
+          <h4>Treinar é se divertir</h4>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img class="d-block w-100" src="img/banner2.png?auto=yes&bg=777&fg=555&text=Second slide" alt="Second slide">
+        <div class="carousel-caption d-none d-md-block">
+          <h3><strong>Cross 373</strong></h3>
+          <h4>Treinar é se Superar</h4>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img class="d-block w-100" src="img/banner3.png?auto=yes&bg=777&fg=555&text=Third slide" alt="Third slide">
+        <div class="carousel-caption d-none d-md-block">
+          <h3><strong>Cross 373</strong></h3>
+          <h4>Treinar é se desafiar</h4>
+        </div>
+      </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+  <!-- Fim do carrossel -->
+<div class="fraseefeito bg-dark">
+  <hr>
+  <h2>"Treinar é se divertir, se superar e se desafiar!"</h2>
+  <hr>
+</div>
+    <!-- Sobre nós -->
+<div id="sobre" class="sobrenos container-fluid">
+
+  <div class="row">
+
+  <div class="textosobre col-lg-8">
+    <div class="titulosobre">
+      <h2>Sobre a Cross 373</h2>
+    </div>
+    <hr>
+    <div class="txtsobre">
+      <p>Somos mais que uma academia convencional , somos um centro de treinamento. Temos a melhor estrutura de Treinamento Funcional e Lutas da região. Tudo para garantir o melhor treino para nossos alunos.</p>
+      <p>Com uma equipe de qualidade e profissionais certificados, somos perfeitos para você! Seja qual for seu objetivo, nós vamos alcança-los juntos!</p>
+    </div>
+  </div>
+  <div class="imgsobre col-lg-4">
+    <br>
+  </div>
+  </div>
+
+</div>
+<div id="horarios" class="parallaxhorarios ">
+
+  <div class="textohorarios">
+      <h2>Horários</h2>
+      <hr>
+      <p>Este é um parágrafo sobre os horários do Cross 373</p>
+  </div>
+</div>
+
+<div class="container">
+<div class="row">
+  <div class="col-xs-12 col-sm-6">
+    <div class="menu-section">
+      <h2 class="menu-section-title">Manhã</h2>
+      <hr>
+          <br>
+      <div class="modalidades">
+        <div class="modhorario"> 06:00 </div>
+
+        <div class="descricaomod"> Crosstraining </div>
+      </div>
+      <div class="modalidades">
+        <div class="modhorario"> 07:00 </div>
+
+        <div class="descricaomod"> Crosstraining </div>
+      </div>
+      <div class="modalidades">
+        <div class="modhorario"> 08:00 </div>
+
+        <div class="descricaomod"> Crosstraining </div>
+      </div>
+      <br>
+
+        <button type="button" class= "btn btn-outline-dark" name="button">Agendar Experimental</button>
+
+
+
+    </div>
+  </div>
+  <div class="col-xs-12 col-sm-6">
+    <div class="menu-section">
+      <h2 class="menu-section-title">Noite</h2>
+      <hr>
+          <br>
+      <div class="modalidades">
+        <div class="modhorario"> 18:00 </div>
+
+        <div class="descricaomod"> Crosstraining </div>
+      </div>
+      <div class="modalidades">
+        <div class="modhorario"> 19:00 </div>
+
+        <div class="descricaomod"> Crosstraining </div>
+      </div>
+      <div class="modalidades">
+        <div class="modhorario"> 20:00 </div>
+
+        <div class="descricaomod"> Crosstraining </div>
+      </div>
+      <br>
+
+        <button type="button" class= "btn btn-outline-dark" name="button">Agendar Experimental</button>
+
+    </div>
+  </div>
+</div>
+</div>
+  <div class="parallaxgaleria">
+
+    <div class="textogaleria">
+        <h2>Galeria</h2>
+        <hr>
+        <p>Fotos do Cross 373</p>
+    </div>
+  </div>
+<div class="galeria container-fluid">
+
+
+  <div class="row">
+    <div class="col-lg-4">
+      <div class="card" style="width:400px">
+        <img class="card-img-top rounded" src="img/cross1.jpg" alt="Card image">
+        <div class="card-body">
+          <h4 class="card-title">Crosstraining na Cross 373</h4>
+          <p class="card-text">Prática com Kettlebell.</p>
+
+        </div>
+      </div>
+      <div class="card" style="width:400px">
+  <img class="card-img-top rounded" src="img/galeria1.jpg" alt="Card image">
+  <div class="card-body">
+    <h4 class="card-title">Crosstraining na Cross 373</h4>
+    <p class="card-text">Prática com Kettlebell.</p>
+
+  </div>
+</div>
+    </div>
+    <div class="col-lg-4">
+      <div class="card" style="width:400px">
+        <img class="card-img-top rounded" src="img/cross2.jpg" alt="Card image">
+        <div class="card-body">
+          <h4 class="card-title">Crosstraining na Cross 373</h4>
+          <p class="card-text">Prática com Kettlebell.</p>
+
+        </div>
+      </div>
+      <div class="card" style="width:400px">
+  <img class="card-img-top rounded" src="img/galeria2.jpg" alt="Card image">
+  <div class="card-body">
+    <h4 class="card-title">Crosstraining na Cross 373</h4>
+    <p class="card-text">Prática com Kettlebell.</p>
+
+  </div>
+</div>
+    </div>
+    <div class="col-lg-4">
+      <div class="card" style="width:400px">
+        <img class="card-img-top rounded" src="img/cross3.jpg" alt="Card image">
+        <div class="card-body">
+          <h4 class="card-title">Crosstraining na Cross 373</h4>
+          <p class="card-text">Prática com Kettlebell.</p>
+
+        </div>
+      </div>
+      <div class="card" style="width:400px">
+  <img class="card-img-top rounded" src="img/galeria3.jpg" alt="Card image">
+  <div class="card-body">
+    <h4 class="card-title">Crosstraining na Cross 373</h4>
+    <p class="card-text">Prática com Kettlebell.</p>
+
+  </div>
+</div>
+    </div>
+</div>
+</div>
+
+
+<footer class="container-fluid">
+    <div class="newsletter row">
+      <div class="textonews col-lg-5">
+          <p>Inscreva-se na nossa newsletter!</p>
+      </div>
+
+      <div class="col-lg-7">
+        <form action="">
+          <div class="camposnewsletter">
+            <input type="text" placeholder="Name" name="name" required>
+            <input type="text" placeholder="Email address" name="mail" required>
+            <input class="btn btn-outline-light" type="submit" value="Inscreva-se">
+          </div>
+        </form>
+      </div>
+      </div>
+
+      <div class="social">
+
+        <img class="socialicon" src="img/instagram.png" alt="email para o cross373" width="40">
+        <img class="socialicon" src="img/facebook.png" alt="email para o cross373" width="35">
+        <img src="img/email.png" alt="email para o cross373" width="50">
+      </div>
+      <hr>
+      <br>
+      <div class="contatos">
+        <div class="row">
+          <div class="col-lg-6">
+            <button class="btn btn-danger" type="button" name="button">Agendar Free Pass </button>
+          </div>
+          <div class="col-lg-6">
+            <button class="btn btn-danger" type="button" name="button">Entre em Contato</button>
+          </div>
+
+        </div>
+      </div>
+      <hr>
+      <br>
+      <p>Telefone: (11) xxxxx-xxxx</p>
+      <p>Endereço: Av. Nossa Sra. da Encarnação, 373. Jardim Maria Estela</p>
+      <br>
+      <hr>
+      <br>
+
+
+<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14620.619127815446!2d-46.5999536!3d-23.6346273!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x90450a21241cce54!2sAcademia+Nishida+Nata%C3%A7%C3%A3o!5e0!3m2!1spt-BR!2sbr!4v1536523586941" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+</footer>
+
+
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+  </body>
+</html>
---
+++ store.php
@@ -0,0 +1,210 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Store Cross 373</title>
+  </head>
+  <body>
+  <!-- Menu -->
+  <nav class="navbar fixed-top navbar-expand-lg navbar-dark  ">
+    <a class="navbar-brand" href="#">CROSS 373</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="index.php#sobre">Sobre</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="index.php#horarios">Horários</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="store.php" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Store
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="#">Planos</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="#">Produtos Fitness</a>
+          </div>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="contato.php">Contato</a>
+        </li>
+      </ul>
+      <ul class="navbar-nav mr-right">
+        <li class="nav-item">
+          <a class="nav-link" href="#">Cadastre-se</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Login</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+
+  <!-- Banner -->
+      <div class="bannerstore">
+        <div class="bannerbg">
+        <hr>
+            <h2 id="titulobanner"><hr>Escolha seu Plano agora!</h2>
+        <hr>
+      </div>
+    </div>
+  <!-- Fim do Banner -->
+<div class="galeria container-fluid">
+
+
+  <div class="row">
+    <div class="col-lg-2" style="padding:0">
+        <div class="menustore rounded" >
+          <form class="form-inline my-2 my-lg-0">
+             <input class="form-control mr-sm-2" type="search" placeholder="Search" style="width:100%; margin:5px">
+             <button class="btn btn-outline-light my-2 my-sm-0" style="width:50%; margin: auto" type="submit">Search</button>
+          </form>
+        <div class="titulomenu">
+          <br>
+          <p> <strong>PLANOS DISPONÍVEIS</strong> </p>
+          <br>
+          <hr>
+        </div>
+          <ul class="listamenu">
+            <li>CrossTraining</li>
+            <li><hr></li>
+            <li>Planos de Crédito</li>
+            <li><hr></li>
+            <li>Planos Mensais</li>
+            <li><hr></li>
+            <li>Planos Semestrais</li>
+            <li><hr></li>
+            <li>Planos Recorrentes</li>
+            <li><hr></li>
+          </ul>
+      </div>
+    </div>
+    <div class="menublank col-lg-1" style="padding:0">
+
+      <br>
+    </div>
+    <div class="col-lg-3" style="padding:0">
+      <div class="card">
+        <img class="card-img-top rounded img-thumbnail" src="img/cross1.jpg" alt="Card image">
+        <div class="card-body">
+          <h4 class="card-title">Crosstraining na Cross 373</h4>
+          <p class="card-text">Prática com Kettlebell.</p>
+
+        </div>
+      </div>
+      <div class="card">
+        <img class="card-img-top rounded img-thumbnail" src="img/cross1.jpg" alt="Card image">
+        <div class="card-body">
+          <h4 class="card-title">Crosstraining na Cross 373</h4>
+          <p class="card-text">Prática com Kettlebell.</p>
+
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-3" style="padding:0">
+      <div class="card">
+        <img class="card-img-top rounded img-thumbnail" src="img/cross2.jpg" alt="Card image">
+        <div class="card-body">
+          <h4 class="card-title">Crosstraining na Cross 373</h4>
+          <p class="card-text">Prática com Kettlebell.</p>
+
+        </div>
+      </div>
+      <div class="card">
+  <img class="card-img-top rounded img-thumbnail" src="img/cross2.jpg" alt="Card image">
+  <div class="card-body">
+    <h4 class="card-title">Crosstraining na Cross 373</h4>
+    <p class="card-text">Prática com Kettlebell.</p>
+
+  </div>
+</div>
+    </div>
+    <div class="col-lg-3" style="padding:0">
+      <div class="card">
+        <img class="card-img-top rounded img-thumbnail" src="img/cross3.jpg" alt="Card image">
+        <div class="card-body">
+          <h4 class="card-title">Crosstraining na Cross 373</h4>
+          <p class="card-text">Prática com Kettlebell.</p>
+
+        </div>
+      </div>
+      <div class="card">
+  <img class="card-img-top rounded img-thumbnail" src="img/cross3.jpg" alt="Card image">
+  <div class="card-body">
+    <h4 class="card-title">Crosstraining na Cross 373</h4>
+    <p class="card-text">Prática com Kettlebell.</p>
+
+  </div>
+</div>
+    </div>
+</div>
+</div>
+
+
+<footer class="container-fluid">
+    <div class="newsletter row">
+      <div class="textonews col-lg-5">
+          <p>Inscreva-se na nossa newsletter!</p>
+      </div>
+
+      <div class="col-lg-7">
+        <form action="">
+          <div class="camposnewsletter">
+            <input type="text" placeholder="Name" name="name" required>
+            <input type="text" placeholder="Email address" name="mail" required>
+            <input class="btn btn-outline-light" type="submit" value="Inscreva-se">
+          </div>
+        </form>
+      </div>
+      </div>
+
+      <div class="social">
+
+        <img class="socialicon" src="img/instagram.png" alt="email para o cross373" width="40">
+        <img class="socialicon" src="img/facebook.png" alt="email para o cross373" width="35">
+        <img src="img/email.png" alt="email para o cross373" width="50">
+      </div>
+      <hr>
+      <br>
+      <div class="contatos">
+        <div class="row">
+          <div class="col-lg-6">
+            <button class="btn btn-danger" type="button" name="button">Agendar Free Pass </button>
+          </div>
+          <div class="col-lg-6">
+            <button class="btn btn-danger" type="button" name="button">Entre em Contato</button>
+          </div>
+
+        </div>
+      </div>
+      <hr>
+      <br>
+      <p>Telefone: (11) xxxxx-xxxx</p>
+      <p>Endereço: Av. Nossa Sra. da Encarnação, 373. Jardim Maria Estela</p>
+      <br>
+      <hr>
+      <br>
+
+
+<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14620.619127815446!2d-46.5999536!3d-23.6346273!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x90450a21241cce54!2sAcademia+Nishida+Nata%C3%A7%C3%A3o!5e0!3m2!1spt-BR!2sbr!4v1536523586941" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+</footer>
+
+
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+  </body>
+</html>
