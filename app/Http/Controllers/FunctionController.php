02ced2ec8fb57dea780c2af47c4983d3264e27ee
Natasha Nishida <nat.nishida@gmail.com>
2018-11-27 12:52:11 -0200

    criação do modelo produto e controller novo

---
+++ app/Http/Controllers/FunctionController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
+use \App\Produto as Produto;
+use \App\User;
+
+class FunctionController extends Controller
+{
+
+}
